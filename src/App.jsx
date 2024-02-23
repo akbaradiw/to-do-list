5701b54de52213d7b5d885d9e8d251dbdb7cb795
akbaradiw <akbaradiw000@gmail.com>
2024-02-23 20:52:47 +0700

    delete some feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import TodoWrapper from "./components/TodoWrapper";
 import FormComp from "./components/FormComp";
 import ListComp from "./components/ListComp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
